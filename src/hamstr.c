f3577b0df901a37bae4ecbf7c33e0d2182ab9c68
Konrad Siek <konrad.siek@gmail.com>
2018-08-21 11:00:04 +0200

    Adding support for NA values.

--- src/hamstr.c
+++ src/hamstr.c
@@ -6,34 +6,51 @@
 // of the old vector increased by one. It only works for REALSXP and INTSXP 
 // vectors.
 SEXP hamstr_increment(SEXP vector) {
-  // Create a new vector of the same type and length as the old vector.
-  SEXP result = Rf_allocVector(TYPEOF(vector), XLENGTH(vector));
+    // Create a new vector of the same type and length as the old vector.
+    SEXP result = Rf_allocVector(TYPEOF(vector), XLENGTH(vector));
 
-  // Check type of the old vector.
-  switch (TYPEOF(vector)) {
-    case INTSXP:
-      // Iterate over the vectors.
-      for (int i = 0; i < XLENGTH(vector); i++)
-        // Retrieve an integer element from the old vector with INTEGER_ELT, 
-        // add one to the result, and set it as an element of the new vector 
-        // with SET_INTEGER_ELT.
-        SET_INTEGER_ELT(result, i, INTEGER_ELT(vector, i) + 1);
+    // Check type of the old vector.
+    switch (TYPEOF(vector)) {
+        case INTSXP:
+            // Iterate over the vectors.
+            for (int i = 0; i < XLENGTH(vector); i++) {
+                // Retrieve an integer element at index i from the old vector.
+                int element = INTEGER_ELT(vector, i);
 
-      return result;
+                // Check if the element is NA.
+                if (element == NA_INTEGER) 
+                    // If the element is NA, just copy it to the result.
+                    SET_INTEGER_ELT(result, i, element);
+                else
+                    // Otherwise add one to the element, and set it as an 
+                    // element of the new vector.
+                    SET_INTEGER_ELT(result, i, element + 1);
+            }
 
-    case REALSXP:
-      // Iterate over the vectors.
-      for (int i = 0; i < XLENGTH(vector); i++)
-	// Retrieve a double element from the old vector with REAL_ELT, add one
-        // to the result, and set it as an element of the new vector with 
-        // SET_REAL_ELT.
-        SET_REAL_ELT(result, i, REAL_ELT(vector, i) + 1);
+            return result;
 
-      return result;
+         case REALSXP:
+            // Iterate over the vectors.
+            for (int i = 0; i < XLENGTH(vector); i++) {
+                // Retrieve a double/real element at index i from the old 
+                // vector.
+                double element = REAL_ELT(vector, i);
 
-    default:
-      // If the vector was neither an INTSXP nor a REALSXP, return R's null 
-      // object.
-      return R_NilValue;
-  }
+                // Check if the element is NA.
+                if (element == NA_REAL) 
+                    // If the element is NA, just copy it to the result.
+                    SET_REAL_ELT(result, i, element);
+                else
+                    // Otherwise add one to the element, and set it as an 
+                    // element of the new vector.
+                    SET_REAL_ELT(result, i, element + 1);
+            }
+
+            return result;
+
+        default:
+            // If the vector was neither an INTSXP nor a REALSXP, return R's 
+            // null object.
+            return R_NilValue;
+    }
 }
